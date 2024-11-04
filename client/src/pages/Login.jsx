eca24bdf5adf2c6e92b2339f8443dc7ab5aab4f6
Pranab Bhowmik <109957935+Pranab09122002@users.noreply.github.com>
2024-11-04 12:05:42 +0530

    Enhance Login component with form handling and input state management; update CSS for input width

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Login = () => {
-  return <div>Login</div>;
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+  const handelInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+  return (
+    <>
+      <section>
+        <main>
+          <div className="section-registration">
+            <div className="container grid grid-two-cols">
+              <div className="registration-image">
+                <img
+                  src="../../public/images/login.png"
+                  alt="Registion pictrure"
+                  width="500"
+                  height="500"
+                />
+              </div>
+              <div className="registration-form">
+                <h1 className="main-heading mb-3">User login from </h1>
+                <br />
+                <form onSubmit={handelSubmit}>
+                  <div>
+                    {/* Entering the enail */}
+                    <label htmlFor="email">email</label>
+                    <input
+                      type="text"
+                      name="email"
+                      placeholder="Enter Email id"
+                      id="email"
+                      required
+                      autoComplete="off"
+                      value={user.email}
+                      onChange={handelInput}
+                    />
+                    {/* Enter Password */}
+                    <label htmlFor="password">password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="Enter password "
+                      id="password"
+                      required
+                      autoComplete="off"
+                      value={user.password}
+                      onChange={handelInput}
+                    />
+                  </div>
+                  <br />
+                  <button type="submit" className="btn btn-submit">
+                    Log in
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </>
+  );
 };
