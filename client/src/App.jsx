8e61154e0d6c3e7f15ce4c792bf1032a82b995fd
Pranab Bhowmik <109957935+Pranab09122002@users.noreply.github.com>
2024-11-04 20:13:31 +0530

    Add Error page component with 404 message and navigation links; update routing in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,6 +24,7 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Logout />} />
+          <Route path="/*" element={<Error />} />
         </Routes>
         <Footer />
       </BrowserRouter>
