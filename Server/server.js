b1723a31e5867a4138ced8b1c7e9595c7672b8af
Pranab Bhowmik <109957935+Pranab09122002@users.noreply.github.com>
2024-11-04 20:51:26 +0530

    Add CORS support and update Register component for form submission; modify package.json and package-lock.json for new dependencies

--- Server/server.js
+++ Server/server.js
@@ -3,13 +3,19 @@ const express = require("express");
 const connectDB = require("./utils/db");
 const authRouter = require("./router/auth_router");
 const contactRouter = require("./router/contact-router");
+const cors = require("cors");
 // Import the auth controller
 
 const errorMiddleware = require("./middleware/error-middelware");
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: "GET, POST,PUT,DELETE, PATCH,HEAD",
+  Credentials: true,
+};
 
 const app = express();
 const port = 3000;
-
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/from", contactRouter);
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 export const Register = () => {
+  const navigate = useNavigate();
   const [user, setUser] = useState({
     username: "",
     email: "",
     phone: "",
     password: "",
   });
+
   const handelInput = (e) => {
     let name = e.target.name;
     let value = e.target.value;
@@ -17,9 +19,29 @@ export const Register = () => {
     });
   };
   // handel Submit form
-  const handelSubmit = (e) => {
+  const handelSubmit = async (e) => {
     e.preventDefault();
-    console.log(user);
+    try {
+      const response = await fetch(`http://localhost:3000/api/auth/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+      console.log(response);
+      if (response.ok) {
+        setUser({
+          username: "",
+          email: "",
+          phone: "",
+          password: "",
+        });
+        navigate("/");
+      }
+    } catch (error) {
+      console.log("registion error", error);
+    }
   };
   return (
     <>
