b0bc0020065d0340e77e862b7de17586f0256935
Pranab Bhowmik <109957935+Pranab09122002@users.noreply.github.com>
2024-11-16 17:22:01 +0530

    Add admin middleware and update admin router to enforce admin access control

--- Server/router/admin-router.js
+++ Server/router/admin-router.js
@@ -3,8 +3,10 @@ const {
   getAllUser,
   getAllContacts,
 } = require("../controllers/admin-controller");
+const authMiddleware = require("../middleware/auth-Middleware");
+const adminMiddleware = require("../middleware/admin-middleware");
 const router = express.Router();
 
-router.route("/users").get(getAllUser);
-router.route("/contacts").get(getAllContacts);
+router.route("/users").get(authMiddleware, adminMiddleware, getAllUser);
+router.route("/contacts").get(authMiddleware, adminMiddleware, getAllContacts);
 module.exports = router;
